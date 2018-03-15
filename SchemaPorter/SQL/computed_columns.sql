e1ed34949f43b5e2635d8b271bc4dab3de4c6c63
Me <me@no.spam>
2018-03-15 19:13:59 +0100

    Persisted

--- SchemaPorter/SQL/computed_columns.sql
+++ SchemaPorter/SQL/computed_columns.sql
@@ -1,4 +1,34 @@
 ﻿
+
+SELECT 
+     sch.name
+    ,tables.name 
+    ,sc.name 
+    ,sc.collation_name 
+    ,sc.is_computed 
+    ,col.[definition] AS Formula
+    ,sc.is_filestream
+    ,sc.is_identity 
+    ,sc.is_nullable
+    ,col.is_persisted
+
+    ,*
+FROM sys.columns AS sc 
+INNER JOIN sys.tables AS tables ON sc.object_id = tables.object_id 
+INNER JOIN sys.schemas AS sch ON sch.schema_id = tables.schema_id
+INNER JOIN sys.computed_columns AS col ON col.object_id = sc.object_id
+WHERE sc.is_computed = 1
+
+
+
+
+
+
+
+
+
+
+
 --SELECT COLUMN_NAME ,
 --COLUMNPROPERTY(OBJECT_ID(TABLE_NAME),COLUMN_NAME,'IsComputed'),
 --COLUMNPROPERTY(OBJECT_ID(TABLE_NAME),COLUMN_NAME,'Formula'),
@@ -13,25 +43,32 @@ SELECT st.name,sc.name
 -- COLUMNPROPERTY(OBJECT_ID(TABLE_NAME),COLUMN_NAME,'IsDeterministic')
 
 ,sc.*
-FROM sys.columns sc 
-INNER JOIN sys.tables st on sc.object_id=st.object_id 
-INNER JOIN sys.schemas sch on sch.schema_id = st.schema_id
+FROM sys.columns AS sc 
+INNER JOIN sys.tables AS st ON sc.object_id = st.object_id 
+INNER JOIN sys.schemas AS sch ON sch.schema_id = st.schema_id
 
 WHERE is_computed = 1
 
 --CREATE THE SCRIPT TO DROP COMPUTED COLUMN
-SELECT 'Alter table ' + st.name  + ' Drop column ' +  sc.name FROM sys.columns sc INNER JOIN sys.tables st on sc.object_id=st.object_id
+SELECT 'Alter table ' + st.name  + ' Drop column ' +  sc.name 
+FROM sys.columns AS sc 
+INNER JOIN sys.tables AS st 
+  ON sc.object_id=st.object_id
 WHERE is_computed = 1
 
 
-SELECT [name], [definition]
+SELECT [name], definition
 FROM sys.computed_columns
 
 
 
-SELECT obj.[name] AS [TableName], col.[name] AS [ColumnName], col.[definition] AS [Formula]
+SELECT 
+   obj.[name] AS TableName
+  ,col.[name] AS ColumnName
+  ,col.[definition] AS Formula
 FROM sys.computed_columns AS col
-INNER JOIN sys.objects AS obj ON col.object_id = obj.object_id
+INNER JOIN sys.objects AS obj 
+  ON col.object_id = obj.object_id
 ORDER BY obj.name, col.name
 
 
@@ -42,11 +79,11 @@ ORDER BY obj.name, col.name
 SELECT * 
 FROM sys.columns sc 
 -- INNER JOIN sys.objects AS obj ON obj.object_id = sc.object_id 
-INNER JOIN sys.tables st on sc.object_id=st.object_id 
-INNER JOIN sys.schemas sch on sch.schema_id = st.schema_id
+INNER JOIN sys.tables AS st ON sc.object_id=st.object_id 
+INNER JOIN sys.schemas AS sch ON sch.schema_id = st.schema_id
 
 INNER JOIN sys.computed_columns AS col 
-	ON col.object_id = sc.object_id 
-    --INNER JOIN sys.objects AS obj ON col.object_id = obj.object_id
+  ON col.object_id = sc.object_id 
+  --INNER JOIN sys.objects AS obj ON col.object_id = obj.object_id
 
 WHERE sc.is_computed = 1
