060a3c2b2eaafa8368fc93b277d7baa8ca4229cb
ststeiger <quandary82@hailmail.net>
2018-03-15 17:37:04 +0100

    computed columns

--- SchemaPorter/SQL/computed_columns.sql
+++ SchemaPorter/SQL/computed_columns.sql
@@ -14,11 +14,39 @@ SELECT st.name,sc.name
 
 ,sc.*
 FROM sys.columns sc 
-inner join sys.tables st on sc.object_id=st.object_id 
-inner join sys.schemas sch on sch.schema_id = st.schema_id
+INNER JOIN sys.tables st on sc.object_id=st.object_id 
+INNER JOIN sys.schemas sch on sch.schema_id = st.schema_id
 
 WHERE is_computed = 1
 
 --CREATE THE SCRIPT TO DROP COMPUTED COLUMN
-SELECT 'Alter table ' + st.name  + ' Drop column ' +  sc.name FROM sys.columns sc inner join sys.tables st on sc.object_id=st.object_id
+SELECT 'Alter table ' + st.name  + ' Drop column ' +  sc.name FROM sys.columns sc INNER JOIN sys.tables st on sc.object_id=st.object_id
 WHERE is_computed = 1
+
+
+SELECT [name], [definition]
+FROM sys.computed_columns
+
+
+
+SELECT obj.[name] AS [TableName], col.[name] AS [ColumnName], col.[definition] AS [Formula]
+FROM sys.computed_columns AS col
+INNER JOIN sys.objects AS obj ON col.object_id = obj.object_id
+ORDER BY obj.name, col.name
+
+
+
+
+
+
+SELECT * 
+FROM sys.columns sc 
+-- INNER JOIN sys.objects AS obj ON obj.object_id = sc.object_id 
+INNER JOIN sys.tables st on sc.object_id=st.object_id 
+INNER JOIN sys.schemas sch on sch.schema_id = st.schema_id
+
+INNER JOIN sys.computed_columns AS col 
+	ON col.object_id = sc.object_id 
+    --INNER JOIN sys.objects AS obj ON col.object_id = obj.object_id
+
+WHERE sc.is_computed = 1
