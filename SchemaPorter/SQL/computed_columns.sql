a5da92a410a367d5109be328b9fe7c78a80927fc
Me <me@no.spam>
2018-03-16 08:30:44 +0100

    Better columns

--- SchemaPorter/SQL/computed_columns.sql
+++ SchemaPorter/SQL/computed_columns.sql
@@ -93,24 +93,59 @@ WHERE sc.is_computed = 1
 
 
 
-
 SELECT 
      sch.name AS schema_name 
     ,tables.name AS table_name 
     ,sc.name AS column_name 
+    ,sc.column_id 
+    ,sm.text AS default_value
     ,sc.collation_name AS collation_name 
+    --,sc.
     ,sc.is_computed 
-    ,col.[definition] AS computation_definition 
+    ,col.[definition] AS computation_definition
+     
     ,col.is_persisted
     ,sc.is_identity 
     ,sc.is_filestream 
     ,sc.is_nullable
-    
+    ,sc.is_sparse 
+    ,t.name 
+    --,t.is_user_defined
+    --,t.is_table_type 
+    --,t.is_assembly_type 
+    ,sc.*
+    -- sc.max_length, sc.precision, sc.scale
 
-    -- ,*
+ , CASE 
+    WHEN TYPE_NAME(sc.system_type_id) IN ('NCHAR','NVARCHAR') 
+    THEN sc.max_length / 2 
+    ELSE sc.max_length 
+  END AS CHARACTER_MAXIMUM_LENGTH 
+
+
+	,CONVERT(tinyint, CASE -- int/decimal/numeric/real/float/money
+		WHEN sc.system_type_id IN (48, 52, 56, 59, 60, 62, 106, 108, 122, 127) THEN sc.precision
+		END) AS NUMERIC_PRECISION
+
+	,CONVERT(smallint, CASE	-- int/money/decimal/numeric
+		WHEN sc.system_type_id IN (48, 52, 56, 60, 106, 108, 122, 127) THEN 10
+		WHEN sc.system_type_id IN (59, 62) THEN 2 END)	AS NUMERIC_PRECISION_RADIX	-- real/float
+
+	,CONVERT(int, CASE	-- datetime/smalldatetime
+		WHEN sc.system_type_id IN (40, 41, 42, 43, 58, 61) THEN NULL
+		ELSE ODBCSCALE(sc.system_type_id, sc.scale) END)	AS NUMERIC_SCALE 
+
+    ,CONVERT(smallint, CASE -- datetime/smalldatetime
+		WHEN sc.system_type_id IN (40, 41, 42, 43, 58, 61) THEN ODBCSCALE(sc.system_type_id, sc.scale) END
+        )	AS DATETIME_PRECISION
+	
+
 FROM sys.columns AS sc 
 INNER JOIN sys.tables AS tables ON sc.object_id = tables.object_id 
 INNER JOIN sys.schemas AS sch ON sch.schema_id = tables.schema_id
-INNER JOIN sys.computed_columns AS col ON col.object_id = sc.object_id
+LEFT JOIN sys.computed_columns AS col ON col.object_id = sc.object_id AND col.column_id = sc.column_id 
+LEFT JOIN sys.syscomments AS sm ON sm.id = sc.default_object_id 
+LEFT JOIN sys.types AS t ON sc.user_type_id = t.user_type_id
 
-WHERE sc.name = '_BE_Label' 
+-- WHERE tables.name = 'T_Benutzer' 
+WHERE tables.name = 'T_Admin' 
